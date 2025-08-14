d2a98b790cf40fc6c6f1e687c924254f3be39954
Nopphadol217 <nopphadols2548@gmail.com>
2025-08-15 00:32:27 +0700

    key firebase changed

--- client/src/lib/firebaseConfig.js
+++ client/src/lib/firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAX01a0nnhKNnlKswlG35iOt86k3AFHnms",
-  authDomain: "loginmern-53e3b.firebaseapp.com",
-  projectId: "loginmern-53e3b",
-  storageBucket: "loginmern-53e3b.firebasestorage.app",
-  messagingSenderId: "772767093873",
-  appId: "1:772767093873:web:5b9868176addc452cd37d0",
-  measurementId: "G-2NGFT46K9T",
+  apiKey: "AIzaSyD0_hhgfgXXONq9JWtKBNvhZbRown5tckQ",
+  authDomain: "bookylife-d3499.firebaseapp.com",
+  projectId: "bookylife-d3499",
+  storageBucket: "bookylife-d3499.firebasestorage.app",
+  messagingSenderId: "992375258827",
+  appId: "1:992375258827:web:4bd53b47bbd7abadc5e54e",
+  measurementId: "G-X689WYFR6H"
 };
 
 const app = initializeApp(firebaseConfig);

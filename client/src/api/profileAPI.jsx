13bffcaeccb440203f2aace4e0eb86ff4b72fedb
Nopphadol217 <nopphadols2548@gmail.com>
2025-08-16 12:14:10 +0700

    test api

--- client/src/api/profileAPI.jsx
+++ client/src/api/profileAPI.jsx
@@ -2,7 +2,7 @@ import api from "@/lib/api";
 import axios from "axios";
 
 export const profileAPI = async (token) => {
-  return await api.get("http://localhost:5000/api/profile", {
+  return await api.get(`${process.env.VITE_API_URL}/api/profile`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
@@ -10,5 +10,5 @@ export const profileAPI = async (token) => {
 };
 
 export const readUserAPI = async () =>{
-  return await axios.get(`http://localhost:5000/api/manage-users`)
+  return await axios.get(`${process.env.VITE_API_URL}/api/manage-users`)
 }

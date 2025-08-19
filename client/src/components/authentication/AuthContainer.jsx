7f4fe8702d60e39a6f6f49a08e3c0514960d0fbd
Nopphadol217 <nopphadols2548@gmail.com>
2025-08-19 17:49:57 +0700

    -

--- client/src/components/authentication/AuthContainer.jsx
+++ client/src/components/authentication/AuthContainer.jsx
@@ -132,8 +132,8 @@ function AuthContainer() {
               <div
                 className={`absolute inset-0 bg-cover bg-center bg-no-repeat ${
                   isLogin
-                    ? "bg-[url('/public/background/login_background.png')]"
-                    : "bg-[url('/public/background/register_background.png')]"
+                    ? "bg-[url(/background/login_background.png')]"
+                    : "bg-[url('/background/register_background.png')]"
                 }`}
               ></div>
 
